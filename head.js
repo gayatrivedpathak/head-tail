53d38e78c0211596cdbad7c07cc7da773184fffa
Gayatri Vedpathak <99533431+gayatrivedpathak@users.noreply.github.com>
2022-05-24 19:48:55 +0530

    Used exit code in head.js

--- head.js
+++ head.js
@@ -1,13 +1,10 @@
-// console.log('usage: head [-n lines | -c bytes] [file ...]');
 const fs = require('fs');
 const { headMain } = require('./src/headLib.js');
 
 const main = () => {
-  try {
-    console.log(headMain(fs.readFileSync, ...process.argv.slice(2)));
-  } catch (err) {
-    console.error('usage: head [-n lines | -c bytes] [file ...]');
-  }
+  const loggers = { stdOut: console.log, stdErr: console.error };
+  const exitCode = headMain(fs.readFileSync, loggers, ...process.argv.slice(2));
+  process.exit(exitCode);
 };
 
 main();
